314464be1d21dd9fae67735bc80777bce8d9b8b5
deepikatyagi84-ux <deepikatyagi84@gmail.com>
2026-09-07 21:05:41 +0530

    Update backend server

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Employee Portfolio Backend is Running");
+    res.end("Employee Portfolio Backend is Running - Developer 2");
 });
 
 server.listen(3000, () => {
