a647bebb58675ea0333a59387c226fceb9238e40
deepikatyagi84-ux <deepikatyagi84@gmail.com>
2026-09-07 20:48:48 +0530

    Add backend server

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Employee Portfolio Backend is Running");
+});
+
+server.listen(3000, () => {
+    console.log("Backend running on http://localhost:3000");
+});
